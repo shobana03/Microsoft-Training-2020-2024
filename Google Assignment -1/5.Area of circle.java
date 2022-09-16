58c7ca7473c351a7e42ee07259e2306645e310ee
shobana03 <100608093+shobana03@users.noreply.github.com>
2022-09-16 20:50:10 +0530

    Create 5.Area of circle.java

---
+++ Google Assignment -1/5.Area of circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    double radius=sc.nextDouble();
+    double area=Math.PI*radius*radius;
+    System.out.println(area);
+    
+  }
+}
