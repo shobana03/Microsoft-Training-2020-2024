b6f6ea2d969ef03242113f1c537fb1a83a5411ae
shobana03 <100608093+shobana03@users.noreply.github.com>
2022-09-18 11:58:26 +0530

    Create 16.Transpose of 2D array.java

---
+++ Google Assignment -1/16.Transpose of 2D array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    int N=sc.nextInt();
+    int arr[][]=new int[N][N];
+    for(int i=0;i<N;i++){
+      for(int j=0;j<N;j++){
+        arr[i][j]=sc.nextInt();
+      }
+    }
+    for(int i=0;i<N;i++){
+      for(int j=0;j<N;j++){
+        System.out.print(arr[j][i]+" ");
+      }
+      System.out.println();
+    }
+    
+  }
+}
