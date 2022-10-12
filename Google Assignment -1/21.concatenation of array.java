eede276b783c29c591bf0065635bd8bf7a0382af
shobana03 <100608093+shobana03@users.noreply.github.com>
2022-10-12 16:01:58 +0530

    Create 21.concatenation of array.java

---
+++ Google Assignment -1/21.concatenation of array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int N=nums.length;
+        int ans[]=new int[2*N];
+        for(int i=0;i<N;i++){
+            ans[i]=ans[i+N]=nums[i];
+            }
+        return ans;
+    }
+}
