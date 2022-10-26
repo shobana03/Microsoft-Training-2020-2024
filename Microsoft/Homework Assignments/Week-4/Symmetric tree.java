bdb2d98fba5fb1d06b8ccce36cfe3c5a4ca8339c
shobana03 <100608093+shobana03@users.noreply.github.com>
2022-10-26 10:33:39 +0530

    Create Symmetric tree.java

---
+++ Microsoft/Homework Assignments/Week-4/Symmetric tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return symmetric(root.left,root.right);
+    }
+    public boolean symmetric(TreeNode left,TreeNode right){
+        if(left==null && right==null){
+            return true;
+        }
+        if(left!=null && right!=null){
+            return (left.val==right.val) && symmetric(left.left,right.right) && symmetric(left.right,right.left);
+        }
+        return false;
+    }
+}
