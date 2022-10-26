21b42cd8071104f5bef4944de8bb6b3b7cbfb8d1
shobana03 <100608093+shobana03@users.noreply.github.com>
2022-10-26 10:53:57 +0530

    Create Merge K sorted Lists.java

---
+++ Microsoft/Homework Assignments/Week-7/Merge K sorted Lists.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length==0)
+        {
+            return null;
+        }
+        ListNode head=new ListNode(0);
+        ListNode temp=head;
+        while(true)
+        {
+            int p=0;
+            for(int i=0;i<lists.length;i++)
+            {
+                if(lists[p]==null || lists[i]!=null && lists[p].val > lists[i].val)
+                    p=i;
+            }
+            if(lists[p]==null)
+            {
+                break;
+            }
+            temp.next=lists[p];
+            temp=temp.next;
+            lists[p]=lists[p].next;
+        }
+        return head.next;
+    }
+}
