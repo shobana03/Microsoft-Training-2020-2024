0f41c03538798dd2a65bf4baf08470e6b149270b
shobana03 <100608093+shobana03@users.noreply.github.com>
2022-11-01 15:41:15 +0530

    Create 56.MinStack.java

---
+++ Question bank problems/Google/56.MinStack.java
@@ -0,0 +1,29 @@
+class MinStack {
+    Stack<Integer>stack=new Stack();
+    Stack<Integer>min_vals=new Stack();
+    
+    public void push(int x) {
+        if(min_vals.isEmpty()||x<=min_vals.peek()){
+            min_vals.push(x);
+        }
+        stack.push(x);
+        
+    }
+    
+    public void pop() {
+        if(stack.peek().equals(min_vals.peek())){
+             min_vals.pop();
+        }
+        stack.pop();
+       
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return min_vals.peek();
+    }
+}
