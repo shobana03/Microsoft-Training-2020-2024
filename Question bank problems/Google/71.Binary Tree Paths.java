bf004f788d6c161e174c06e12e647a74b808f6a0
shobana03 <100608093+shobana03@users.noreply.github.com>
2022-11-01 15:49:59 +0530

    Create 71.Binary Tree Paths.java

---
+++ Question bank problems/Google/71.Binary Tree Paths.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans=new ArrayList<String>();
+        if(root!=null){
+            FormPath(root,"",ans);
+        }
+        return ans;
+    }
+    public void FormPath(TreeNode root,String path,List<String>ans){
+        if(root.left==null && root.right==null){
+            ans.add(path+root.val);
+        }
+        if(root.left!=null) FormPath(root.left,path+root.val + "->",ans);
+        if(root.right!=null) FormPath(root.right,path+root.val + "->",ans);
+    }
+}
